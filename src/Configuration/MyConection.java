1963e9717497933b838868b557eca0831c215006
Jesus Borda <jesusdeveloper2314@gmail.com>
2025-01-27 07:45:41 -0500

    Coreccion error local de librerias

--- src/Configuration/MyConection.java
+++ src/Configuration/MyConection.java
@@ -12,22 +12,24 @@ import java.sql.SQLException;
  * @author prueba
  */
 public class MyConection {
-    private static final String URL = "jdbc:mysql://localhost:3308/crud_java_mvc";
+    private static final String URL = "jdbc:mysql://localhost:3306/crud_java_mvc";
     private static final String USER = "root";
-    private static final String PASSWORD = "";
+    private static final String PASSWORD = "Contraseña";
     
     public static Connection getConnection (){
         Connection connection = null;
         try {
             //CARGAR EL DRIVER DE Mysql
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
             //Establecer la conexion
             connection = DriverManager.getConnection(URL, USER, PASSWORD);
             System.out.println("Conexion exitosa a la base de datos");
         } catch (ClassNotFoundException e) {
             System.out.println("Error al cargar el driver de MYSQL" + e.getMessage());
+             e.printStackTrace(); //
             
         } catch (SQLException e) {
+            e.printStackTrace();
             System.err.println("Eror al conectar a la base de datos" + e.getMessage());
         }
         return connection;
